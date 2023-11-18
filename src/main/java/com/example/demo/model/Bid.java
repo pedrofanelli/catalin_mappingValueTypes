4c1b395ee4622e6637d5c740c801af3addd316a9
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-18 16:58:00 -0300

    bid model

---
+++ src/main/java/com/example/demo/model/Bid.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Bid {
+
+	@Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    private Long id;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY) // NOT NULL
+    @JoinColumn(name = "ITEM_ID") // Actually the default name
+    private Item item;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
