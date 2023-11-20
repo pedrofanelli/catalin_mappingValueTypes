c26f18f39d7bbdae2d2de4d581bc1224159a3f01
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-20 17:05:47 -0300

    converter

---
+++ src/main/java/com/example/demo/converter/MonetaryAmountConverter.java
@@ -0,0 +1,20 @@
+package com.example.demo.converter;
+
+import com.example.demo.model.MonetaryAmount;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class MonetaryAmountConverter implements AttributeConverter<MonetaryAmount, String> {
+
+	@Override
+    public String convertToDatabaseColumn(MonetaryAmount monetaryAmount) {
+        return monetaryAmount.toString();
+    }
+
+    @Override
+    public MonetaryAmount convertToEntityAttribute(String s) {
+        return MonetaryAmount.fromString(s);
+    }
+}
