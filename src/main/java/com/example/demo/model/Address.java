995f13f3c9898e6f410cc86dd88244ac0dc1c9fb
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-20 15:43:01 -0300

    building address pojo as embeddable

---
+++ src/main/java/com/example/demo/model/Address.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Instead of <code>@Entity</code>, this component POJO is marked with <code>@Embeddable</code>. It
+ * has no identifier property.
+ * 
+ * No tiene ID porque no está representado en la base de datos, son propiedades que se suman a las
+ * que posea la clase a la cual este POJO se "une" o "embeba"
+ */
+@Embeddable
+public class Address {
+
+    @NotNull // Ignored for DDL generation!
+    @Column(nullable = false) // Used for DDL generation!
+    private String street;
+
+    @NotNull
+    @Column(nullable = false, length = 5) // Override VARCHAR(255)
+    private String zipcode;
+
+    @NotNull
+    @Column(nullable = false)
+    private String city;
+
+    /**
+     * Hibernate will call this no-argument constructor to create an instance, and then
+     * populate the fields directly.
+     */
+    public Address() {
+    }
+
+    /**
+     * You can have additional (public) constructors for convenience.
+     */
+    public Address(String street, String zipcode, String city) {
+        this.street = street;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- src/main/java/com/example/demo/package-info.java
+++ src/main/java/com/example/demo/package-info.java
@@ -1,21 +1,5 @@
 
-/**
- * 
- * In a Spring Boot project, the package-info.java file is typically used to define package-level 
- * annotations and documentation. This file can be placed in any package to provide additional information 
- * about the package and its contents.
- * 
- * Tenemos la generación de una entidad en la base usando 2 métodos, Hibernate/JPA y Spring Data
- * Spring Data también usa hibernate y jpa, pero permite evitar boilercode a cambio de rendimiento
- * En Spring Data tenemos la configuración que se realiza una vez, ya sea en un archivo .properties o en la configuracion de
- * beans; luego armamos repositorios con los métodos y chau.
- * En cambio, Hibernate/JPA puro necesitamos configurar un persistence.xml o una clase custom que forme el 
- * persistence unit, y luego en cada lugar que se vaya a usar debemos escribir mucho más codigo, formando
- * el entity manager, y escribiendo las queries, no tenemos la facilidad de los repositorios.
- * 
- * @author peter
- *
- */
+
 @org.hibernate.annotations.GenericGenerator( // un generador CUSTOM de valor automático, donde la estrategia ahora se llama "type"
   name = "ID_GENERATOR",
   //strategy = "enhanced-sequence",
